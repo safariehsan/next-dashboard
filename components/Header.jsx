4a8690663d66353febb80be0e47a34f83c07bf99
Ehsan Safari <ehsansafari@hotmail.com>
2023-02-11 12:26:48 +0330

    add sidebar component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between px-4 pt-4">
+      <h2>Next Dashboard</h2>
+      <h3>Welcome back, client</h3>
+    </div>
+  );
 };
 
 export default Header;
---
+++ components/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = ({ children }) => {
+  return <div>Sidebar{children}</div>;
+};
+
+export default Sidebar;
