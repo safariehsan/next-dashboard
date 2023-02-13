eb4c7c2c109b07b14d8e019634b0dd82ba003641
rahkarino <ehsansafari@hotmail.com>
2023-02-13 12:41:29 +0330

    add recent-orders component

---
+++ pages/customers.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BsPersonFill, BsThreeDotsVertical } from "react-icons/bs";
+
+const customers = () => {
+  return <div>customers</div>;
+};
+
+export default customers;
